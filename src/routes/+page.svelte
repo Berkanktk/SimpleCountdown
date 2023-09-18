b8556b5cc2974885b824097fb33b4ca5bef390f3
Berkanktk <berkank@live.dk>
2023-09-19 00:50:32 +0200

    Improved user experience

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,67 +37,71 @@
 
 </script>
 
-<main class="min-h-screen items-center justify-center bg-gradient-to-r from-black to-gray-900 text-white p-8 rounded-lg">
-  
+<main class="flex min-h-screen items-center justify-center bg-gradient-to-r from-black to-gray-900 text-white p-8 rounded-lg h-screen">
+
   <div class="mb-4 flex flex-col space-y-4 w-1/2 mx-auto">
-    <h1 class="text-5xl mb-4">Countdown</h1>
-
-    <div class="relative rounded-md shadow-sm">
-      <input
-        
-        type="text"
-        disabled={isRunning}
-        bind:value={eventName}
-        class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
-        placeholder="Graduation..."
-      />
-    </div>
+    <!-- <h1 class="text-5xl mb-4">Countdown</h1> -->
     
-    <div class="relative rounded-md shadow-sm">
-      <input
-        disabled={isRunning}
-        type="date"
-        bind:value={date}
-        class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
-      />
-    </div>
-
-    <div class="relative rounded-md shadow-sm">
-      <input
-        disabled={isRunning}
-        type="time"
-        bind:value={time}
-        class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
-      />
-    </div>
-    
-    {#if isError}
-      <div class="alert alert-error">
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Enter information.</span>
+    {#if !isRunning}
+      <div class="relative rounded-md shadow-sm">
+        <input
+          
+          type="text"
+          disabled={isRunning}
+          bind:value={eventName}
+          class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
+          placeholder="Graduation..."
+        />
       </div>
-    {/if}
+      
+      <div class="relative rounded-md shadow-sm">
+        <input
+          disabled={isRunning}
+          type="date"
+          bind:value={date}
+          class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
+        />
+      </div>
 
-    <button disabled={isRunning} on:click={setTargetDate} class="btn btn-primary w-full">Set Countdown</button>
-
-    {#if isRunning}
-        <button on:click={clearAll} class="btn btn-secondary w-full">Reset</button>
-    {/if}
-
-    {#if isSuccessful}
-      <div class="toast">
-        <div class="alert alert-success">
-          <span>Countdown set successfully!</span>
+      <div class="relative rounded-md shadow-sm">
+        <input
+          disabled={isRunning}
+          type="time"
+          bind:value={time}
+          class="form-input block w-full py-2 px-3 rounded-md bg-gray-800 text-white"
+        />
+      </div>
+      
+      {#if isError}
+        <div class="alert alert-error">
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>Enter information.</span>
         </div>
-      </div>
-    {/if}
-    
-    {#if targetDate}
-      <Countdown targetDate={targetDate} eventName={eventName} />
-      {#if isFinished}
-        <button on:click={clearAll} class="btn btn-secondary w-1/2 mx-auto">Reset</button>
       {/if}
+
+      <button disabled={isRunning} on:click={setTargetDate} class="btn btn-primary w-full">Start Countdown</button>
+
     {/if}
-  </div>
+      
+      {#if targetDate}
+        <Countdown targetDate={targetDate} eventName={eventName} />
+        {#if isFinished}
+          <button on:click={clearAll} class="btn btn-secondary w-1/2 mx-auto">Reset</button>
+        {/if}
+      {/if}
+
+      {#if isRunning}
+          <button on:click={clearAll} class="btn btn-secondary w-full">Reset</button>
+      {/if}
+    </div>
 
 </main>
+
+
+{#if isSuccessful}
+  <div class="toast">
+    <div class="alert alert-success">
+      <span>Countdown set successfully!</span>
+    </div>
+  </div>
+{/if}
